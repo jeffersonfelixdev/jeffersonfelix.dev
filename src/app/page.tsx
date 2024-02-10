6fe46db0817c2248e5c859506178de71368dba5c
Jefferson Felix <jefferson@felixtech.dev>
2024-02-10 00:37:53 -0300

    fix: add pipe instead commas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,15 @@ export default function Home() {
       </ul>
       <h2 className="font-semibold text-lg">Dev skills</h2>
       <ul className="p-4 flex flex-col gap-4">
-        <li> Languages: HTML/CSS/Javascript/ECMAScript/TypeScript</li>
+        <li> Languages: HTML | CSS | Javascript | ECMAScript/TypeScript</li>
         <li> Backend: Node.js (Express.js and NestJS)</li>
         <li> Frontend: React and Next.js</li>
         <li> Mobile: React Native</li>
-        <li> ORM: Sequelize, TypeORM, Prisma, Mongoose, Dynamoose</li>
-        <li> CMS: Strapi, Contentful, Prismic, Ghost</li>
-        <li> DB: PostgreSQL, MySQL, Oracle, MongoDB, DynamoDB, Redis</li>
+        <li> ORM: Sequelize | TypeORM | Prisma | Mongoose | Dynamoose</li>
+        <li> CMS: Strapi | Contentful | Prismic | Ghost</li>
+        <li> DB: PostgreSQL | MySQL | Oracle | MongoDB | DynamoDB | Redis</li>
         <li> Cloud: AWS</li>
-        <li> CI/CI: Docker, CircleCI, kubernetes, Jenkins, Terraform</li>
+        <li> CI/CI: Docker | CircleCI | kubernetes | Jenkins | Terraform</li>
       </ul>
     </main>
   )
